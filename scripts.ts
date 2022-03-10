b7f8bb12378bb9f0c9d07409196eda8f466f532c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-03-10 18:04:14 +0100

    add scripts file for velociraptor

---
+++ scripts.ts
@@ -0,0 +1,9 @@
+// Config docs: https://velociraptor.run/docs/configuration/
+
+const scripts = {
+    // short form command for `deno run file.ts` 
+    start: "main.ts"
+
+}
+
+export default scripts;
