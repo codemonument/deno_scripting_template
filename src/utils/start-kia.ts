2a5126168a4a58a17049b8a3e66f8ec1cfe7d4b4
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-03-14 21:50:10 +0100

    upgrade Kia from 0.1.0 to 0.4.1 and add some descriptive text to the startKia wrapper function

--- src/utils/start-kia.ts
+++ src/utils/start-kia.ts
@@ -1,5 +1,12 @@
-import { Kia } from "https://deno.land/x/kia@v0.1.0/kia.ts";
+import { Kia } from "https://deno.land/x/kia@0.4.1/mod.ts";
 
+/**
+ * Starts an entry on cli with a spinner
+ * Could be used, for example, to show the "working" state for one action
+ * To build a log of actions and their statuses, call startKia multiple times per action which should be logged
+ * @param text
+ * @returns
+ */
 export async function startKia(text: string): Promise<Kia> {
   const kia = new Kia(text);
   await kia.start();
