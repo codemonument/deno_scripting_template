f0f3d85f97baf5add285ca9693747a54c4aa6a9c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-03-14 22:03:10 +0100

    delete dependencies file and use new resolveCwd.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,17 @@
-import { log, resolveToAbsolutePath } from "./src/dependencies.ts";
 import { startKia } from "./src/utils/startKia.ts";
+import { resolveCwd } from "./src/utils/resolveCwd.ts";
 
-let CWD = Deno.env.get("CWD");
-if (CWD) {
-  CWD = resolveToAbsolutePath(CWD);
-  log.info(`Using current working directory from CWD Env var: ${CWD}`);
-} else {
-  CWD = Deno.cwd();
-  log.info(`Using current working directory from Deno.cwd(): ${CWD}`);
-}
+const CWD = resolveCwd();
 
 /**
  * VERSION: 1.0.0
  */
 try {
-  const demoKia = await startKia(`Base Deno Repo, Sleep for 3 sek`);
+  const demoKia = await startKia(
+    `Base Deno Repo, Sleep for 3 sek`,
+  );
   setTimeout(async () => {
-    await demoKia.succeed(`Finished Base Deno Repo sucessfully`);
+    await demoKia.succeed(`Finished Base Deno Repo sucessfully, CDW: ${CWD}`);
   }, 3000);
 } catch (error) {
   console.error(error);
--- src/dependencies.ts
+++
@@ -1,2 +0,0 @@
-export * as log from "https://deno.land/std@0.129.0/log/mod.ts";
-export { resolve as resolveToAbsolutePath } from "https://deno.land/std@0.129.0/path/mod.ts";
