ba3e0973b0ed6ce626e008020a2641e0a6edad08
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 18:49:44 +0200

    make implementatin of main command more straightforward

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,5 @@
 // Follows the pattern at
 // https://github.com/yargs/yargs/blob/main/docs/advanced.md#example-command-hierarchy-using-indexmjs
 
-import * as defaultCommand from './default.ts';
-export const commands = [defaultCommand];
+import * as mainCommand from "./main.ts";
+export const commands = [mainCommand];
