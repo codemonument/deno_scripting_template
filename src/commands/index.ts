8ba4b8244a588dbfb5d5a3048ecbd9de12019baa
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-01 19:22:36 +0200

    fix mainCommand import

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,5 @@
 // Follows the pattern at
 // https://github.com/yargs/yargs/blob/main/docs/advanced.md#example-command-hierarchy-using-indexmjs
 
-import * as mainCommand from "./main.ts";
+import * as mainCommand from "./mainCommand.ts";
 export const commands = [mainCommand];
